fbc37e90c1963c27878a75669b98485d75b09497
tapanshah13 <tapanshah1327@gmail.com>
2024-09-17 18:57:20 -0700

    Lab 1

---
+++ Day 1/timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for Chicago</h1>
+      <h2>Welcome to Chicago!</h2>
+      <Calendar />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Day 1/timetabled/src/components/Calendar.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import Event from './Event'
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>8 AM</td>
+                        <Event event='Starbucks ☕️' color ='green' location='Maple & Ash'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Yolk 🍳' color ='green'/>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>9 AM</td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Subway 🚊' color ='pink'/>
+                        <td></td>
+                        <td></td>
+                        <Event event='The Bean 🫘' color ='blue'/>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>10 AM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>11 AM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>12 PM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>1 PM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>2 PM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>3 PM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>4 PM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>5 PM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Calendar;
